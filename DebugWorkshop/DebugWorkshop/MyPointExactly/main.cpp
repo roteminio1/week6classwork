966da7395adde87c0661a9158aad8eae2296b932
roteminio1 <rotemlulfel@gmail.com>
2025-12-01 20:06:17 +0200

    added all the parts until SafeAndSound

--- DebugWorkshop/DebugWorkshop/MyPointExactly/main.cpp
+++ DebugWorkshop/DebugWorkshop/MyPointExactly/main.cpp
@@ -1,14 +1,15 @@
 #include "Point.h"
 #include <iostream>
 
-//  a.the code need to print p1 and p2 
-// b. i expected to see the valus of p1 and p2
-// c. the debugger didnt stopped because it didnt print the object in main
+//  1a.the code need to print p1 and p2 
+// 1b. i expected to see the valus of p1 and p2
+// 1c. the debugger didnt stopped because it didnt print the object in main
+
 int main()
 {
+	//3a
 	Point p1(1, 2);
-	Point p2;
-	p2 = p1;
+	Point p2 = p1;
 
 	std::cout << p1 << std::endl; // fixed
 	std::cout << p2 << std::endl; // fixed
